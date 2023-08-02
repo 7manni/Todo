657239b619107f2e3deb35e1c591ad341435e84c
Abdulrahman Atta <abood.taef@gmail.com>
2023-08-02 16:10:29 +0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Abdulrahman Atta on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
